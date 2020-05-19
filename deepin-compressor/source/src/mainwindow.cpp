86cfaf866ea83d04c41db0cb9fa6dc98b7887428
chenglu <chenglu@uniontech.com>
2020-05-19 16:53:05 +0800

    Type:fix
    Description:extract .7z file at uncompresspage will display progress page
    
    RootCause:Null
    
    Solution:judge extract type is not EXTRACT_SINGLE

--- deepin-compressor/source/src/mainwindow.cpp
+++ deepin-compressor/source/src/mainwindow.cpp
@@ -1379,11 +1379,12 @@ void MainWindow::slotExtractionDone(KJob *job)
 
 void MainWindow::slotShowPageUnzipProgress()
 {
-    m_pageid = PAGE_UNZIPPROGRESS;
-    refreshPage();
-    m_progressdialog->setProcess(0);
-    m_Progess->setprogress(0);
-
+    if (m_encryptiontype != Encryption_SingleExtract) {
+        m_pageid = PAGE_UNZIPPROGRESS;
+        refreshPage();
+        m_progressdialog->setProcess(0);
+        m_Progess->setprogress(0);
+    }
 }
 
 void MainWindow::SlotNeedPassword()
