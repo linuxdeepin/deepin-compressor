bb5fd92d1c3bb5c1a87a06e788c5f594b52eb317
chendu <chendu@uniontech.com>
2020-07-31 09:40:36 +0800

    feat: 取消时删除临时解压文件，放到回收站
    Description: 取消时删除临时解压文件，放到回收站
    
    Log: 取消时删除临时解压文件，放到回收站
    
    link:

--- deepin-compressor/source/inc/mainwindow.h
+++ deepin-compressor/source/inc/mainwindow.h
@@ -274,10 +274,10 @@ private:
     void deleteCompressFile(/*QStringList oldfiles, QStringList newfiles*/);
 
     /**
-     * @brief deleteDecompressFile    关闭/取消/退出时删除临时解压文件
+     * @brief deleteDecompressFile    关闭/取消/退出时删除临时解压文件，放到回收站
      * @param destDirName
      */
-    void deleteDecompressFile(QString destDirName = "");
+    void deleteDecompressFile(QString destDirName = QString(""));
 
     /**
      * @brief startCmd  启动命令
