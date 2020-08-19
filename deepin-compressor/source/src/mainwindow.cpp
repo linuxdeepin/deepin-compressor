d0247d2bbb3cba07fa052bce23f84c18c60f8404
gaoxiang <gaoxiang@uniontech.com>
2020-08-19 18:50:03 +0800

    fix: 修复zip加密包提取某些情况下不需要再出现密码的问题
    对加密包进行标志位的设置
    
    Log: zip加密包如果第一层只有文件夹，第一次提取其中文件之后，以后再提取不用加密
    
    link:

--- deepin-compressor/source/src/mainwindow.cpp
+++ deepin-compressor/source/src/mainwindow.cpp
@@ -2192,6 +2192,10 @@ void MainWindow::slotShowPageUnzipProgress()
 
 void MainWindow::SlotNeedPassword()
 {
+    if (m_pArchiveModel->archive()->encryptionType() == Archive::Unencrypted) {
+        m_pArchiveModel->archive()->setEncryptionType(Archive::Encrypted);
+    }
+
     if (PAGE_ENCRYPTION != m_ePageID) {
         m_ePageID = PAGE_ENCRYPTION;
         refreshPage();
