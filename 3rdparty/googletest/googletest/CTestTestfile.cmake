7de21ffcac2bdd13210e9300d94274229deace18
yuanxiang.lei <leiyuanxiang@uniontech.com>
2020-09-02 17:22:56 +0800

    chore:添加单元测试第三方库文件
    Description: chore:添加单元测试第三方库文件
    
    Log: chore:添加单元测试第三方库文件
    
    link:

---
+++ 3rdparty/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /data/home/lx777/work_UT/compressor/3rdparty/googletest/googletest
+# Build directory: /data/home/lx777/work_UT/compressor/3rdparty/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
