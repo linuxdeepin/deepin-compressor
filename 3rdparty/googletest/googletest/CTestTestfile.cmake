1a4b44e96d0d73490a9ba889297363c3bbe2af84
yuanxiang.lei <leiyuanxiang@uniontech.com>
2020-09-02 17:47:42 +0800

    fix:删除第三方测试库文件
    Description: 删除第三方测试库文件
    
    Log: 删除第三方测试库文件
    
    link:

--- 3rdparty/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/home/lx777/work_UT/compressor/3rdparty/googletest/googletest
-# Build directory: /data/home/lx777/work_UT/compressor/3rdparty/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
