8427d56f692459e5b35e1436d0e59fd576f04650
yuanxiang.lei <leiyuanxiang@uniontech.com>
2020-09-04 15:02:58 +0800

    fix:将测试工程进行拆分,每个三方插件作为一个测试用例进行测试
    Description: 将测试工程进行拆分,每个三方插件作为一个测试用例进行测试
    
    Log: 将测试工程进行拆分,每个三方插件作为一个测试用例进行测试
    
    link:

---
+++ tests/UnitTest/3rdparty/interface/UT_archiveentry.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include <gtest/src/stub.h>
+#include <archive.h>
+#include <archive_entry.h>
+#include <archive_manager.h>
+
+TEST(Archive_Archive_UT, Archive_Archive_UT001)
+{
+    Archive::Entry *archive = nullptr;
+    //  ArchiveSortFilterModel *model = new ArchiveSortFilterModel();
+    ASSERT_EQ(archive, nullptr);
+    //  delete archive;
+    //   ASSERT_NE(archive, nullptr);
+    //  delete archive;
+}
