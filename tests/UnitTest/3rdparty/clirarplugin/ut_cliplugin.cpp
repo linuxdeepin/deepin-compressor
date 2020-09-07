903572f3c6cc6c8010cbcadc2a439bafa2a33a6f
yuanxiang.lei <leiyuanxiang@uniontech.com>
2020-09-07 16:43:06 +0800

    feat:添加cliRarPlugin单元测试demo
    Description: 构建clirar的单元测试的工程,以及单元测试的demo
    
    Log: 添加cliRarPlugin单元测试demo
    
    link:

---
+++ tests/UnitTest/3rdparty/clirarplugin/ut_cliplugin.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include <gtest/src/stub.h>
+#include "cliplugin.h"
+
+TEST(Archive_CliRarPlugin_UT, Archive_CliRarPlugin_UT001)
+{
+    CliRarPlugin *cliRarPlugin = nullptr;
+    //  ArchiveSortFilterModel *model = new ArchiveSortFilterModel();
+    ASSERT_EQ(cliRarPlugin, nullptr);
+}
