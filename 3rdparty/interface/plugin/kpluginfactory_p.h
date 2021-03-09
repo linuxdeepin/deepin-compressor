aa87f5f0b151138b34ea9d87b912e4bb9a48fa79
chendu <chendu@uniontech.com>
2021-03-09 13:34:19 +0800

    fix: 项目文件缺少部分LICENSE和copyright
    项目文件添加LICENSE和copyright
    
    Log: 项目文件添加LICENSE和copyright
    Change-Id: Ieb154c74a7c5520f88c55cc3cf3aceed9e143362

--- 3rdparty/interface/plugin/kpluginfactory_p.h
+++ 3rdparty/interface/plugin/kpluginfactory_p.h
@@ -1,24 +1,19 @@
-/*
- * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
- *
- * Author:     dongsen <dongsen@deepin.com>
- *
- * Maintainer: dongsen <dongsen@deepin.com>
- *             AaronZhang <ya.zhang@archermind.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*  This file is part of the KDE project
+    Copyright (C) 2007 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2007 Bernhard Loos <nhuh.put@web.de>
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
 
 #ifndef KPLUGINFACTORY_P_H
 #define KPLUGINFACTORY_P_H
--- 3rdparty/interface/plugin/kpluginloader.h
+++ 3rdparty/interface/plugin/kpluginloader.h
@@ -1,24 +1,18 @@
-/*
- * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
- *
- * Author:     dongsen <dongsen@deepin.com>
- *
- * Maintainer: dongsen <dongsen@deepin.com>
- *             AaronZhang <ya.zhang@archermind.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*  This file is part of the KDE project
+    Copyright (C) 2007 Bernhard Loos <nhuh.put@web.de>
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
 #ifndef KPLUGINLOADER_H
 #define KPLUGINLOADER_H
 
