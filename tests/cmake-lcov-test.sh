a5263c42830b6bc2ad2c9ac0cc87de973668a233
gaoxiang <gaoxiang@uniontech.com>
2021-07-12 14:57:16 +0800

    chore: 修改UT生成的xml名称
     修改UT生成的xml名称
    
    Log: 修改UT生成的xml名称
    Change-Id: If24f21a6a95f5d755888640bec3d9d159ecf5532

--- tests/cmake-lcov-test.sh
+++ tests/cmake-lcov-test.sh
@@ -8,19 +8,19 @@ cd ../$utdir
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j16
 
-./bin/tests/bz2plugin_test --gtest_output=xml:./report/report1.xml
-./bin/tests/cli7zplugin_test --gtest_output=xml:./report/report2.xml
-./bin/tests/clirarplugin_test --gtest_output=xml:./report/report3.xml
-./bin/tests/deepin-compressor_test --gtest_output=xml:./report/report4.xml
-./bin/tests/gzplugin_test --gtest_output=xml:./report/report5.xml
-./bin/tests/interface_test --gtest_output=xml:./report/report6.xml
-./bin/tests/libarchive_test --gtest_output=xml:./report/report7.xml
-./bin/tests/libminizipplugin_test --gtest_output=xml:./report/report8.xml
-./bin/tests/libzipplugin_test --gtest_output=xml:./report/report9.xml
-./bin/tests/readonlylibarchiveplugin_test --gtest_output=xml:./report/report10.xml
-./bin/tests/readwritelibarchiveplugin_test --gtest_output=xml:./report/report11.xml
-./bin/tests/singlefile_test --gtest_output=xml:./report/report12.xml
-./bin/tests/xzplugin_test --gtest_output=xml:./report/report13.xml
+./bin/tests/bz2plugin_test --gtest_output=xml:./report/report_bz2plugin.xml
+./bin/tests/cli7zplugin_test --gtest_output=xml:./report/report_cli7zplugin.xml
+./bin/tests/clirarplugin_test --gtest_output=xml:./report/report_clirarplugin.xml
+./bin/tests/deepin-compressor_test --gtest_output=xml:./report/report_deepin-compressor.xml
+./bin/tests/gzplugin_test --gtest_output=xml:./report/report_gzplugin.xml
+./bin/tests/interface_test --gtest_output=xml:./report/report_interface.xml
+./bin/tests/libarchive_test --gtest_output=xml:./report/report_libarchive.xml
+./bin/tests/libminizipplugin_test --gtest_output=xml:./report/report_libminizipplugin.xml
+./bin/tests/libzipplugin_test --gtest_output=xml:./report/report_libzipplugin.xml
+./bin/tests/readonlylibarchiveplugin_test --gtest_output=xml:./report/report_readonlylibarchiveplugin.xml
+./bin/tests/readwritelibarchiveplugin_test --gtest_output=xml:./report/report_readwritelibarchiveplugin.xml
+./bin/tests/singlefile_test --gtest_output=xml:./report/report_singlefile.xml
+./bin/tests/xzplugin_test --gtest_output=xml:./report/report_xzplugin.xml
 
 workdir=$(cd ../$(dirname $0)/$utdir; pwd)
 
