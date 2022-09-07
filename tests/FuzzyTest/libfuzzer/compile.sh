f0327c6143531350503d1795dc5dd3f35d24d872
luominjiang <luominjiang@uniontech.com>
2022-09-07 13:35:02 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- tests/FuzzyTest/libfuzzer/compile.sh
+++ tests/FuzzyTest/libfuzzer/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 mkdir -p build
 cd build
 cmake .. -DCMAKE_C_COMPILER=/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/opt/llvm/bin/clang++
