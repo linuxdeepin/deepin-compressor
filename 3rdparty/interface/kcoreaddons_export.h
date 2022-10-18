d77a48538c0310e628695e9a372edef653f6ba2a
luominjiang <luominjiang@uniontech.com>
2022-10-18 13:49:48 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- 3rdparty/interface/kcoreaddons_export.h
+++ 3rdparty/interface/kcoreaddons_export.h
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
 #ifndef KCOREADDONS_EXPORT_H
 #define KCOREADDONS_EXPORT_H
 
